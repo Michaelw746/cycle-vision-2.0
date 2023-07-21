6e57f300c5cdad05690f4d4fafe2b83056068b7e
TXT-84 <txt.hackathon@TXTs-MacBook-Pro-6.local>
2023-07-21 15:03:02 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,155 @@
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  ReactNativeZoomableView,
+  onPress,
+  Image,
+} from "react-native";
+// this file is using for creating a tracker and to use expo map api for the map
+import React, { useState, useEffect } from "react";
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
+import { Marker } from "react-native-maps";
+import { Card, SearchBar } from "react-native-elements";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { Switch } from "react-native-elements";
+import { TouchableOpacity } from "react-native";
+
+export default function App({ navigation }) {
+  let [mapRegion, setMapRegion] = useState({
+    latitude: 34.034411637144196,
+    longitude: -118.45671197410529,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  console.log("Initial lat", mapRegion.latitude);
+  console.log("Initial Long", mapRegion.longitude);
+
+  const [location, setLocation] = useState(null);
+  // Request user location info
+  const userLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+    }
+    {
+      /*Gets current position and then sets the latitude and longitude*/
+    }
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+      timeInterval: 1,
+    });
+
+    setLocation(location);
+    setMapRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+    console.log("This is lat", location.coords.latitude);
+    console.log("This is long", location.coords.longitude);
+  };
+
+  useEffect(() => {
+    userLocation();
+  }, []);
+  const [isEnabled, setIsEnabled] = useState(false);
+  const [imageSource, setImageSource] = useState(
+    require("./assets/unlocker.png")
+  );
+
+  const toggleSwitch = () => {
+    setIsEnabled((previousState) => !previousState);
+    setImageSource(
+      isEnabled
+        ? require("./assets/unlocker.png")
+        : require("./assets/locker.png")
+    );
+  };
+
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <View>
+      {/* <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="pin"
+            component={pinnumber}
+            style={styles.button}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>  */}
+      <View style={styles.button}>
+        <Image source={require("./assets/logo.png")} style={styles.logo} />
+        <Text style={styles.boot}>Boot</Text>
+      </View>
+
+      <MapView style={styles.map} region={mapRegion}>
+        <Marker coordinate={mapRegion} title="Marker" />
+       
+      </MapView>
+      <View styles={styles.overlayImageContainer}>
+      <Switch
+        style={styles.boot}
+        trackColor={{ false: "#ffffff", true: "#2E3A53" }}
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+        onPress={() => navigation.navigate("Lock")}
+      />
+</View>
+      <TouchableOpacity onPress={() => console.log("Pressed!")}>
+        <Image source={imageSource} />
+      </TouchableOpacity>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "White",
+  },
+  button: {
+    borderRadius: 0,
+    height: 150,
+    backgroundColor: "#FFFFFF",
+    flexDirection: "row", // Align items in a horizontal line
+    margin: 0,
+  },
+  text: {
+    color: "white",
+    fontSize: 24,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+    margin: 0,
+    zIndex:0,
+  },
+  logo: {
+    margin: 0,
+    height: 84,
+    width: 84,
+  },
+  boot: {
+    position:"absolute",
+    alignItems: "center",
+    bottom: 0,
+    zIndex:1,
+  },
+  search: {
+    flex: 1,
+    bottom: 0,
+  },
+  
+});
---
+++ Pinnumber.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const pinnumber = () => {
+  return (
+    <View>
+      <Text>pinnumber</Text>
+    </View>
+  )
+}
+
+export default pinnumber
+
+const styles = StyleSheet.create({})
